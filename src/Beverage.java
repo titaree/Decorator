afc3114fa847b84c6316f646376f295b98235c86
titaree <30823986+titaree@users.noreply.github.com>
2017-08-15 16:04:11 +0700

    Add Beverage Class
    Add attribute, method abstact method

--- src/Beverage.java
+++ src/Beverage.java
@@ -1,4 +1,10 @@
 
 public abstract class Beverage {
+	String description = "Unknow beverage";
 
+	public String getDescription () {
+		return description;
+		
+	}
+	public abstract double cost();
 }
