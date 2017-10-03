5a8fe209bec14a8a4d62ad7461dd938475204884
titaree <30823986+titaree@users.noreply.github.com>
2017-10-03 15:47:04 +0700

    edit num

--- src/StarBuzzCoffee.java
+++ src/StarBuzzCoffee.java
@@ -1,25 +1,27 @@
+import java.text.NumberFormat;
 
 public class StarBuzzCoffee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		NumberFormat numFormat = NumberFormat.getInstance();
+		
        Beverage b1 = new Espresso();
-      System.out.println(b1.getDescription()+ " $"+b1.cost());
+      System.out.println(b1.getDescription()+ " $"+numFormat.format(b1.cost()));
       Beverage b2 = new HouseBlend();
       b2 = new Mocha(b2);
       b2 = new Whip(b2);
-      System.out.println(b2.getDescription()+" $"+b2.cost());
+      System.out.println(b2.getDescription()+" $"+numFormat.format(b2.cost()));
       
       Beverage b3 = new Decaf();
       b3 = new StreamedMilk(b3);
       b3 = new soy(b3);
       b3 = new Whip (b3);
-      System.out.println(b3.getDescription()+" $"+b3.cost());
+      System.out.println(b3.getDescription()+" $"+numFormat.format(b3.cost()));
       
       Beverage b4 = new DarkRoast();
       b4 = new Mocha(b4);
       b4 = new soy(b3);
-      System.out.println(b4.getDescription()+" $"+b4.cost());
+      System.out.println(b4.getDescription()+" $"+numFormat.format(b4.cost()));
 	}
 
 }
